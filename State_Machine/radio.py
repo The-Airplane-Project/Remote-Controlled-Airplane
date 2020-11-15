f2e90c758040e3ed3c6ec5aaf54e05f37202df07
Ayush Ghosh <ayushghosh21@gmail.com>
2020-11-14 23:39:12 -0700

    Fixed indentation error in radio.py for calc_crc8 function

--- State_Machine/radio.py
+++ State_Machine/radio.py
@@ -180,20 +180,20 @@ class radio_comm:
         
         return False
 
-def calc_crc8(self, datagram, initial_value=0):
-    crc = initial_value
-
-    # Iterate bytes in data
-    for byte in datagram:
-        # Iterate bits in byte
-        for _ in range(0, 8):
-            if (crc >> 7) ^ (byte & 0x01):
-                crc = ((crc << 1) ^ 0x07) & 0xFF
-            else:
-                crc = (crc << 1) & 0xFF
-            # Shift to next bit
-            byte = byte >> 1
-    return crc
+    def calc_crc8(self, datagram, initial_value=0):
+        crc = initial_value
+    
+        # Iterate bytes in data
+        for byte in datagram:
+            # Iterate bits in byte
+            for _ in range(0, 8):
+                if (crc >> 7) ^ (byte & 0x01):
+                    crc = ((crc << 1) ^ 0x07) & 0xFF
+                else:
+                    crc = (crc << 1) & 0xFF
+                # Shift to next bit
+                byte = byte >> 1
+        return crc
 
     def send_message(self, msg):
         # grab variables from i2c_sensor, ultrasound, and send
